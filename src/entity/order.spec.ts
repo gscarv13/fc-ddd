311ecb3e35451efa4dfe2dd7a70aa0ed4836bf53
Gustavo Carvalho <gsc13@tuta.io>
2022-11-22 11:09:57 -0300

    Add Order Aggregate

---
+++ src/entity/order.spec.ts
@@ -0,0 +1,33 @@
+import Order from "./order"
+import OrderItem from "./order_item";
+
+describe("Order unit tests", () => {
+  it("should throw error when id is empty", () => {
+    
+    expect(() => {
+      new Order("", "123", [])
+    }).toThrowError("ID is required for Order");
+  });
+
+  it("should throw error when customerId is empty", () => {
+    
+    expect(() => {
+      new Order("321", "", [])
+    }).toThrowError("customerID is required for Order");
+  });
+
+  it("should throw error when OrderItem is less than 1", () => {
+    
+    expect(() => {
+      new Order("321", "123", [])
+    }).toThrowError("The order needs to have at least one item");
+  });
+
+  it("should calculate total", () => {
+    const items = Array.from({ length: 3 }, (_, id) => new OrderItem(`id-${id}`, `item-${id}`, Number(id) * 10))
+    
+    const order = new Order('order-123', 'client-123', items)
+    
+    expect(order.total()).toBe(30);
+  });
+});
---
+++ src/entity/order.ts
@@ -0,0 +1,35 @@
+import OrderItem from "./order_item";
+
+export default class Order {
+  private _id: string;
+  private _customerId: string;
+  private _items: OrderItem[];
+  private _total: number;
+
+  constructor(id: string, customerId: string, items: OrderItem[]) {
+    this._id = id;
+    this._customerId = customerId;
+    this._items = items
+    this._total = this.total();
+    this.validate();
+  }
+
+  validate() {
+    if (this._id.length === 0) {
+      throw new Error("ID is required for Order");
+    }
+    if (this._customerId.length === 0) {
+      throw new Error("customerID is required for Order");
+    }
+    if (this._items.length <= 0) {
+      throw new Error("The order needs to have at least one item");
+    }
+  
+
+    return true;
+  }
+
+  total(): number {
+    return this._items.reduce((total, item) => total + item.price, 0);
+  }
+}
